65e1308e85f183a68ba57de552bde48b8fa9384b
Tomer Vazana <rupcgroup30@gmail.com>
2023-07-17 21:44:21 +0300

    commit

---
+++ client-admin/theme/assets/js/pages/calendar.js
@@ -0,0 +1,31 @@
+//document.addEventListener('DOMContentLoaded', function() {
+//  var calendarEl = document.getElementById('calendar');
+
+//  var calendar = new FullCalendar.Calendar(calendarEl, {
+//    plugins: [ 'interaction', 'dayGrid', 'timeGrid', 'bootstrap'],
+//    timeZone: 'UTC',
+//    header: {
+//      left: 'title',
+//      center: 'dayGridMonth,timeGridWeek,timeGridDay',
+//      right: 'prev,next today'
+//    },
+//    editable: true,
+//    eventLimit: true, // when too many events in a day, show the popover
+//    events: '',
+//    themeSystem: 'bootstrap'
+//  });
+
+//  calendar.render();
+//});
+
+//var event = [
+//    {
+//        title: '????? 1',
+//        start: '2019-08-12T10:30:00',
+//        end: '2019-08-12T11:30:00',
+//        extendedProps: {
+//            department: 'BioChemistry'
+//        },
+//        description: 'Lecture'
+//    }
+//]
